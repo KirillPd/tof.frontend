36d591accdc113e7f72c509e12fa574f0c9b566b
KirillPd <poddubny.kirill@gmail.com>
2018-04-07 19:11:59 +0300

    Delete `CustomStatusBar` component

--- src/components/CustomStatusBar/styles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-import platformInfo from '../../constants/platformInfo';
-
-export default StyleSheet.create({
-  statusBar: {
-    height: platformInfo.STATUSBAR_HEIGHT
-  }
-});
