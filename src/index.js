549ff85d6ea98698ec4238fbf8f80d6c5f2481d2
KirillPd <poddubny.kirill@gmail.com>
2018-04-07 18:10:41 +0300

    Add redux

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { View } from 'react-native';
+// TODO: Add reducers to the store
+// import store from './redux/store';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View />
+    );
+    // TODO: return component wrapped in Provider component
+    // return (
+    //   <Provider store={store}>
+    //     <View />
+    //   </Provider>
+    // );
+  }
+};
